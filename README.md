44337ed26f38d37a574732188801388a125d31e2
2inf <wtyang2000@foxmail.com>
2023-10-15 16:01:49 +0800

    add notes to README

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ Followed:
 [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
 
 use `main > image.ppm` to redirect to an image file.
+
+## Notes
+
+### rendered image setup
+
+`viewport_width`: don't use `aspect_ratio` because it is the ideal ratio and may not be the actual ratio between `image_width` and `image_height`
+
+### ray-sphere intersection
+
+the vector from center $C=(C_x,C_y,C_z)$ to point $P=(x, y, z)$ is $(P-C)$, then the sphere in vector form:
+
+$$
+(P-C)(P-C)=r^2
+$$
+
+we want to know if our ray $P(t)=A+tb$ ever hits the sphere anywhere, looking for any $t$ where this is true:
+
+$$
+((A+tb)-C)((A+tb)-C)=r^2
+$$
+
+
--- main.cc
+++ main.cc
@@ -4,18 +4,26 @@
 
 #include <iostream>
 
-bool hit_sphere(const point2 &center, double radius, const ray &r) {
+double hit_sphere(const point3 &center, double radius, const ray &r) {
     vec3 oc = r.origin() - center;
     auto a = dot(r.direction(), r.direction());
     auto b = 2.0 * dot(oc, r.direction());
     auto c = dot(oc, oc) - radius * radius;
     auto discriminant = b * b - 4 * a * c;
-    return (discriminant >= 0);
+    
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant) / (2.0 * a));
+    }
 }
 
 color ray_color(const ray &r) {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r))
-        return color(1, 0, 0);
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
 
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
@@ -62,6 +70,7 @@ int main() {
 			auto ir = to_int(r), ig = to_int(g), ib = to_int(b);
 			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 			*/
+
             auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
